b058fe6883b0204d6a39e6ec0b629b2eaf895cfa
Tang <out_tang_look@outlook.com>
2018-10-16 10:07:49 +0900

    update

--- src/main/java/SparkTaskSubmitter.java
+++ src/main/java/SparkTaskSubmitter.java
@@ -2,6 +2,9 @@ import org.apache.spark.deploy.SparkSubmit;
 import org.apache.spark.launcher.SparkLauncher;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class SparkTaskSubmitter {
 
@@ -15,38 +18,20 @@ public class SparkTaskSubmitter {
     public static void submit(String[] args) throws Exception {
 
         final String sparkHome = "/Users/TangDexian/spark-2.3.1-bin-hadoop2.7";
+        String[] cmds = new String[]{
+                "--master", "k8s://https://35.229.152.59",
+                "--deploy-mode", "cluster",
+                "--conf", "spark.kubernetes.container.image=geekbeta/spark:v100",
+                "--conf", "spark.kubernetes.authenticate.driver.serviceAccountName=spark",
+                "--conf", "spark.app.name=wtfhappend",
+                "--conf", "spark.executor.instances=3",
+                "--class", "FuckingPI",
+                "--driver-cores", "0.1",
+                "--executor-cores", "1",
+                "--name", "spark-pi",
+                "local:///opt/spark/tang_stuff/spark-demo.jar"
+        };
 
-        SparkSubmit
-
-        SparkLauncher sparkLauncher = new SparkLauncher()
-
-                .setSparkHome(sparkHome)
-
-                .setMaster("k8s://https://35.229.152.59")
-                .setDeployMode("cluster")
-                .setAppName("PI cal")
-                .setMainClass("FuckingPI")
-                .addJar("local:///opt/spark/tang_stuff/spark-demo.jar")
-                .addSparkArg("spark.driver.cores", "0.1")
-                .setConf("spark.executor.instance", "3")
-                .setConf("spark.kubernetes.container.image", "geekbeta/spark:v100")
-                .setConf("spark.kubernetes.authenticate.driver.sericeAccountName", "spark");
-
-
-        Process proc = sparkLauncher.launch();
-
-//        InputStream input = proc.getInputStream();
-//        OutputStream output = proc.getOutputStream();
-//        InputStream error = proc.getErrorStream();
-//
-//        BufferedReader read = new BufferedReader(new OutputS(new BufferedOutputStream(output)));
-//        String content;
-//        while((content = read.readLine()) != null) {
-//            System.out.println(content);
-//        }
-
-        int status = proc.waitFor();
-
-        System.out.println("finished with srtatus code: " + status);
+        SparkSubmit.main(cmds);
     }
 }
